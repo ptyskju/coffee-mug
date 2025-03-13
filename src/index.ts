b2e3ae12c62f17a4c388d8e4017c16509d47069f
Krzysztof Wegrzyn <krzysztof.wegrzyn@flip.shop>
2025-03-13 11:43:54 +0100

    Add dotenv and mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
+import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
+import {connectDB} from "./config/database";
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+async function init() {
+  try {
+    dotenv.config();
+    await connectDB();
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+init();
